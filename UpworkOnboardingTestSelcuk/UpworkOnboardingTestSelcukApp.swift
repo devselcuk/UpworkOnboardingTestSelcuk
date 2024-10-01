894d2154c3fa44f66c8fe263bf2f844aa9241d37
Selcuk <devselcukdev@gmail.com>
2024-10-01 18:56:00 +0200

    Initial Commit

---
+++ UpworkOnboardingTestSelcuk/UpworkOnboardingTestSelcukApp.swift
@@ -0,0 +1,17 @@
+//
+//  UpworkOnboardingTestSelcukApp.swift
+//  UpworkOnboardingTestSelcuk
+//
+//  Created by SELCUK YILDIZ on 01.10.24.
+//
+
+import SwiftUI
+
+@main
+struct UpworkOnboardingTestSelcukApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ UpworkOnboardingTestSelcukTests/UpworkOnboardingTestSelcukTests.swift
@@ -0,0 +1,17 @@
+//
+//  UpworkOnboardingTestSelcukTests.swift
+//  UpworkOnboardingTestSelcukTests
+//
+//  Created by SELCUK YILDIZ on 01.10.24.
+//
+
+import Testing
+@testable import UpworkOnboardingTestSelcuk
+
+struct UpworkOnboardingTestSelcukTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
